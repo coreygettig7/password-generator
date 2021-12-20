5047ed6a6471013a265c1c4c1a16ce744c30f40b
Corey Gettig <coreygettig7@gmail.com>
2021-12-19 22:31:05 -0500

    added generate password function but still have issues

--- script.js
+++ script.js
@@ -6,6 +6,39 @@ const key_strings = {
     symbol: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
 };
 
+function generatePassword() {
+    var passwordCharSet = "";
+  
+    var length = window.prompt("Enter a number from 8 to 128 for password length.");
+  
+    var lowercase = window.confirm("Would you like to use lowercase letters?");
+        if (lowercase) {
+            passwordCharSet += key_strings.lowercase;
+        };
+  
+    var uppercase = window.confirm("Would you like to use uppercase letters?");
+        if (uppercase) {
+            passwordCharSet += key_strings.uppercase;
+        };
+  
+    var symbols = window.confirm("Would you like to use symbols?");
+        if (symbols) {
+            passwordCharSet += key_strings.symbol;
+        };
+  
+    var numbers = window.confirm("Would you like to use numbers?");
+        if (numbers) {
+            passwordCharSet += key_strings.number;
+        };
+    var password = "";
+        for (let i = 0; i < length; i++) {
+            password += passwordCharSet[Math.floor(Math.random() * passwordCharSet.length)]
+        }
+        return password;
+}
+
+generatePassword();
+
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
