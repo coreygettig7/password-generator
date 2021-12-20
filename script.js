cfc6de7ac8162c7d8e16dac8b0e49e281e740ad6
Corey Gettig <coreygettig7@gmail.com>
2021-12-19 22:50:16 -0500

    worked in wrong branch and had to reset back to last merge

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 // Assignment code here
 const key_strings = {
-    lowercase: 'abcdefghijklmnopqrstuvwxyz',
-    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',
-    number: '0123456789',
+    lowercase: 'abcdefghijklmnopqrstuvwxyz',   
+    uppercase: 'ABCDEFGHIJKLMNOPQRSTUVWXYZ',    
+    number: '0123456789',    
     symbol: "!#$%&'()*+,-./:;<=>?@[]^_`{|}~",
 };
 
